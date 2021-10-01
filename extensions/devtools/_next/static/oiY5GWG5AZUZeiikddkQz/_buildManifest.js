823b1b59d988d0c25ccbb8c9885470e808cb3b51
BeLeap <BeLeap@users.noreply.github.com>
2021-10-01 08:05:43 +0000

    [WORKFLOW] Add Exporeted Result

--- extensions/devtools/_next/static/oiY5GWG5AZUZeiikddkQz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-46af3eafeb5b25d2b35f.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
