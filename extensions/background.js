212943be0bac45da4fa0bb26814356ca5c8fa197
Changseo Jang <changseo.jang@riiid.co>
2021-10-01 17:54:22 +0900

    capture request data & response data

--- extensions/background.js
+++ extensions/background.js
@@ -1,8 +1,20 @@
-function logUrl(details) {
-    console.log("Loading: " + details.url);
+function requestListener(details) {
+  let filter = browser.webRequest.filterResponseData(details.requestId);
+  let decoder = new TextDecoder('utf-8');
+
+  console.log(details.requestBody);
+
+  filter.ondata = (event) => {
+    let str = decoder.decode(event.data, { stream: true });
+    console.log(str);
+    filter.disconnect();
+  };
+
+  return {};
 }
 
 browser.webRequest.onBeforeRequest.addListener(
-    logUrl,
-    {urls: ["<all_urls>"]}
+  requestListener,
+  { urls: ['*://*.riiid.cloud/*'] },
+  ['requestBody', 'blocking'],
 );
