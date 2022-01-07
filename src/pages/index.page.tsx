35b3ae6a99e301c6f4a7211aa9ff98319fb23d0c
Yongwook Choi <yongwook.choi@riiid.co>
2022-01-07 15:19:32 +0900

    Add configId on Requests, and apply settings
    Remove unused Event

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from "next";
 import { useEffect } from "react";
+import { useAtom } from "jotai";
 import { useUpdateAtom } from "jotai/utils";
 import style from "./index.module.scss";
 import Events from "../Events";
-import { requests1 } from "./index/mocks/requests";
 import {
   requestsAtom,
   updateRequestAtom,
@@ -14,12 +14,18 @@ import {
 } from "./index/atoms/request";
 import RequestDetail from "./index/RequestDetail";
 import RequestList from "./index/RequestList";
+import { preserveLogAtom } from "./index/atoms/setting";
+import Settings from "./index/Settings";
+import { selectedRequestKeyAtom } from "./index/atoms/ui";
 
 const Page: NextPage = () => {
   useDevtoolsCommunicationLogic();
   return (
     <div className={style.page}>
-      <RequestList />
+      <div className={style.sidebar}>
+        <Settings />
+        <RequestList />
+      </div>
       <RequestDetail />
     </div>
   );
@@ -29,6 +35,12 @@ export default Page;
 
 function useDevtoolsCommunicationLogic() {
   const updateRequests = useUpdateAtom(requestsAtom);
+  const [, setSelectedRequestSpecifier] = useAtom(selectedRequestKeyAtom);
+  const resetRequests = () => {
+    setSelectedRequestSpecifier(undefined);
+    updateRequests({});
+  };
+  const [preserveLog] = useAtom(preserveLogAtom);
   const updateRequest = useUpdateAtom(updateRequestAtom);
   const updateRequestPayload = useUpdateAtom(updateRequestPayloadAtom);
   const updateResponse = useUpdateAtom(updateResponseAtom);
@@ -37,6 +49,7 @@ function useDevtoolsCommunicationLogic() {
   useEffect(() => {
     try {
       const port = chrome.runtime.connect({ name: "@pbkit/devtools/panel" });
+      const tabs = chrome.tabs;
       port.postMessage({ tabId: chrome.devtools.inspectedWindow.tabId });
       interface Message {
         target: string;
@@ -67,9 +80,15 @@ function useDevtoolsCommunicationLogic() {
           }
         }
       });
+      const listener = (_: unknown, changeInfo: chrome.tabs.TabChangeInfo) => {
+        if (changeInfo.status === "loading") {
+          return !preserveLog && resetRequests();
+        }
+      };
+      tabs.onUpdated.addListener(listener);
+      return () => tabs.onUpdated.removeListener(listener);
     } catch {
       console.warn("not running on chrome developer tools");
-      updateRequests(requests1);
     }
-  }, []);
+  }, [preserveLog]);
 }
--- src/pages/index/atoms/ui.ts
+++ src/pages/index/atoms/ui.ts
@@ -1,10 +1,12 @@
 import { atom } from "jotai";
 import { requestsAtom } from "./request";
 
-export const selectedRequestIdAtom = atom<string | undefined>(undefined);
+export const selectedRequestKeyAtom = atom<string | undefined>(undefined);
 
 export const selectedRequestAtom = atom((get) => {
   const requests = get(requestsAtom);
-  const selectedRequestId = get(selectedRequestIdAtom);
-  return selectedRequestId ? get(requests[+selectedRequestId]) : undefined;
+  const selectedRequestKey = get(selectedRequestKeyAtom);
+  if (!selectedRequestKey) return undefined;
+  const selectedRequestAtom = requests[selectedRequestKey];
+  return selectedRequestAtom ? get(selectedRequestAtom) : undefined;
 });
