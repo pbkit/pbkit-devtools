55b37e651495a838c247e2896a44bb63abcd50f7
Yongwook Choi <yongwook.choi@riiid.co>
2022-02-25 12:49:57 +0900

    Add debug req add button on settings

--- src/pages/index/Settings/index.tsx
+++ src/pages/index/Settings/index.tsx
@@ -6,14 +6,19 @@ import Checkbox from "../../../components/Checkbox";
 import { resetRequestsAtom } from "../atoms/ui";
 import { preserveLogAtom } from "../atoms/setting";
 import styles from "./index.module.scss";
+import { useAddMockRequests } from "../mocks/requests";
 
 interface SettingsProps {}
 const Settings: React.FC<SettingsProps> = () => {
   const resetRequests = useUpdateAtom(resetRequestsAtom);
+  const addMockRequests = useAddMockRequests();
   const [preserveLog, setPreserveLog] = useAtom(preserveLogAtom);
   return (
     <div className={styles.settings}>
       <Button onClick={resetRequests}>Remove cache</Button>
+      {process.env.NODE_ENV === "development" && (
+        <Button onClick={() => addMockRequests()}>Add mock reqs</Button>
+      )}
       <Checkbox
         isChecked={preserveLog}
         onChange={(v) => {
