ed3c78d4e9c077b31bff2a319755e9025785782d
Yongwook Choi <hyperflow@kakao.com>
2022-07-06 15:45:57 +0900

    remove standalone and fix action

--- packages/standalone/mod.ts
+++
@@ -1,8 +0,0 @@
-import { open } from "https://deno.land/x/pbkit@v0.0.49/cli/pollapo/misc/browser.ts";
-import { port } from "./port.ts";
-
-new Worker(new URL("./server.ts", import.meta.url).href, {
-  type: "module",
-});
-
-open(`http://localhost:${port}/index.html`);
--- packages/standalone/port.ts
+++
@@ -1,4 +0,0 @@
-import { parse } from "https://deno.land/std@0.146.0/flags/mod.ts";
-
-const args = parse(Deno.args);
-export const port = args.port ?? 8099;
